b47d250c802d957316cf8a26066f9aad4181d69f
Akash <adcharitar@hotmail.com>
2020-02-23 13:12:41 -0800

    Separate files

---
+++ SoA-hot_and_cold_data_split/Node.h
@@ -0,0 +1,36 @@
+#include <iostream> 
+
+class Node
+{
+    public:
+     
+        std::vector<int> x;
+        std::vector< int > y;
+        std::vector< std::string > name;
+        
+
+    Node(){
+    
+    }
+
+
+    void add(int a,int b,std::string c){
+        x.push_back(a);
+        y.push_back(b);
+        name.push_back(c);
+
+    }
+
+};
+
+bool equal(int x1,int y1, std::string n1, int x2,int y2, std::string n2 ){
+
+    if( (x1 == x2) && (y1== y2 )
+
+              && (n1== n2 )  // Test name also - better cache without?
+              
+         )
+        return true;
+
+    return false;
+}

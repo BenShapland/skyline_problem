91080c12283a2ea01117572cfd205057d0cfa68e
Akash <adcharitar@hotmail.com>
2020-04-16 22:09:27 -0700

    Remove dir

--- GPGPU_basic/DATA_gen_test/yvals.hpp
+++
@@ -1,6 +0,0 @@
-#include "Node.h"
-
-std::vector< XY > yvals = { {100,215}, {23,45}};
-
-
-// XY yvals[] = { {69,15} };
--- GPGPU_basic/GPU/data-sanity-check.hpp
+++
@@ -1,5 +0,0 @@
-#include "Node.h"
-// #include <vector>
-XY data_array[16] = {XY{1, 12}, XY{2, 7}, XY{4, 22}, XY{5, 14}, XY{6, 5}, XY{8, 19}, XY{9, 9}, XY{10, 4}, XY{12, 23}, XY{15, 15}, XY{15,22}, XY{16,6}, XY{17,10}, XY{17,20}, XY{21,3}, XY{22,14}};
-
-char char_data[]= "aaaabbbbccccddddeeeeffffgggghhhhiiiijjjjkkkkllllmmmmnnnnoooopppp";
